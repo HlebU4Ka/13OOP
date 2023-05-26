d2401a909b21263c5bda93591406a37a1911dcfb
glebk <glebkot57@gmail.com>
2023-05-26 14:13:39 +0200

    Готово

--- srs/main.py
+++ srs/main.py
@@ -1,17 +1,38 @@
-import requests, json
-from datetime import datetime
+import json
 import os
+from datetime import datetime
 
-API_KEY = os.getenv("EXCHANGE_RATE_API_KEY")
+import requests
 
-def get_currency_rate(base):
-    pass
-def save_to_json(data):
-    pass
+API_KEY = os.getenv("YT_API_KEY")
+CURRENCY_RATES_FILE = "currency_rates.json"
+def get_currency_rate(base: str)-> float:
+    """Получает курс валюты от API и возвращает его в виде float"""
+    url = f"https://api.apilayer.com/exchangerates_data/latest"
+    response = requests.get(url, headers={'apikey': API_KEY}, params={'base': {base}})
+    rate = response.json()['rates']['RUB']
+    return rate
+
+
+
+def save_to_json(data: dict) -> None:
+    """ Сохраняет данные в json файл. """
+    with open(CURRENCY_RATES_FILE, "a") as f:
+        if os.stat(CURRENCY_RATES_FILE).st_size == 0:
+            json.dump([data], f)
+        else:
+            with open(CURRENCY_RATES_FILE) as f:
+                data_list = json.load(f)
+                data_list.append(data)
+            with open(CURRENCY_RATES_FILE, "w") as f:
+                json.dump(data_list, f)
+
+
+
 def main():
     while True:
         currency = input("Введите название валюты (USD или EUR) ")
-        if currency not in ("USD", "EUR"):
+        if currency not in ("USD", "EUR "):
             print("Некоректный ввод")
             continue
             # При не выполненных условиях код ниже пропуститься,
@@ -19,13 +40,13 @@ def main():
 
 
         rate = get_currency_rate(currency)
-        linestamp = datetime.now()
+        linestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
         print(f"Курс {currency} к рублю {rate}")
         data = {"currency": currency, "rate": rate, "linestamp": linestamp}
         save_to_json(data)
 
-        choice = input("Выберете дайствие: 1 - продолжить, 2 - выйти")
+        choice = input("Выберете дайствие: 1 - продолжить, 2 - выйти ")
         if choice == "1":
             continue
         elif choice == "2":
